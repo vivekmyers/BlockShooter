d5ca68cfd1dbc31190d98a971ad05cbf59c6aa39
Vivek Myers <vivek.myers@gmail.com>
2018-05-13 20:43:04 -0400

    .

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ window.onload = main;
 
 function main() {
     const g = new Game();
+    const request = new XMLHttpRequest();
+    const time = new Date();
+    const formatted = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds();
+    request.open("POST", "http://kbam.net/?log=blockshooter-[" + formatted + "]", true);
+    request.send();
     g.load(1);
     window.fps = 60;
     Graphics.start(fps);
